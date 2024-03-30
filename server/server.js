144aa13fc6af6d936450738f22bca94589e6d444
amit04-07-1999 <ameykumar76@gmail.com>
2024-03-30 18:36:39 +0530

    first

--- server/server.js
+++ server/server.js
@@ -130,7 +130,7 @@ app.get('/api/history', async (req, res) => {
 });
 
 
-// Start the server
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
